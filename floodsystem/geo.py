737809b7f038068c88f5c5f8200e992872b39802
ai387 <ai387@cam.ac.uk>
2022-01-23 10:43:40 +0000

    Adding comments to code

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -13,73 +13,20 @@ import stationdata # importing all data abt the stations
 
 list_of_stations = stationdata.build_station_list(True)
 #print(list_of_stations[0])
-list_items = []
 station_name = []
 station_coord = []
 
-
 for each_station in list_of_stations:
-    list_lines = str(each_station).splitlines() # splitting the data in a list, where each item = line
-    counter=0
+    list_lines = str(each_station).splitlines() # splitting data in a list, each component = each line of station info
+    counter=0 # initialising counter to count each line of station info
     for each_line in list_lines:
-        counter += 1
-        items = each_line.split(': ')
-        items = items [1::2]
-        for each_item in items:
-            list_items.append(each_item)
-            if counter == 1:
-                station_name.append(each_item)
-            if counter == 4:
-                station_coord.append(each_item)
-
-
-'''
-
-        for item in items:
-            
-            list_items.append(item)
-        list_items
-        # splitting each line into its separate items
-        counter = 0
-        for item in items:
-            counter += 1
-            if counter == 1:
-                station_name.append(item)
-            elif counter == 4:
-                station_coord.append(item)
-
-print(station_name)
-
-#print(list_lines, '\n', list_items)
-
-'''
-
-'''
-names = []
-
-for item in list_of_stations:
-    counter = 0
-    for i in item:
-        counter += 1
-        if counter == 1:
-            names.append(item[counter])
-        else: continue
-
-
-
-
-
-
-station_id = stationdata
-measure_id
-label =
-coord =
-typical_range
-river
-town
-st.MonitoringStation.__init__()
-'''
-
+        counter += 1 # adding one to counter for each line of station info e.g. 1 is Station name, 2 is id etc...
+        items = each_line.split(': ') # splitting each line into a list (e.g. [Station name, Bourton Dickler])
+        items = items [1::2] # removing list descriptor e.g. removing Station name
+        if counter == 1: # if the item is the first in the list it is a station name
+            station_name.append(items)
+        if counter == 4: # if the item is the fourth in the list it is the station coordinates
+            station_coord.append(items)
 
 #import datafetcher
 #from .station import MonitoringStation
