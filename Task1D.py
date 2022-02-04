efce7a5d6189300f29a88122f1777f42481a7ea5
ai387 <ai387@cam.ac.uk>
2022-02-04 19:46:18 +0000

    little edits to 1D

--- Task1D.py
+++ Task1D.py
@@ -1,14 +1,16 @@
+# ARZINA: RUNNING THIS CODE IN GEO GIVES YOU A DIFFERENT OUTPUT TO WHAT IS EXPECTED (ON THE TASK IT GIVES EXAMPLE OUTPUT,
+# ARZINA: BUT MY OUTPUT IS NOT THE SAME'
 
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
 
-def run1():
+def run():
     stations = build_station_list()
-    print("There are ", len(rivers_with_station(stations)), " stations with at least one monitoring system.")
     output = rivers_with_station(stations)
+    print("There are ", len(output), " stations with at least one monitoring system.")
     print("{}".format(output[:10]))
 
 if __name__ == "__main__":
     print("*** Task 1C: CUED Part IA Flood Warning System ***")
-    run1()
+    run()
