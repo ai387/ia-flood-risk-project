6e00d43083f22ea5162ea7ab25535213da1e4949
yh488 <yh488@cam.ac.uk>
2022-02-05 18:52:43 +0000

    Done the tests up to 1D

--- test_geo.py
+++ test_geo.py
@@ -4,10 +4,16 @@ from tokenize import Number
 from floodsystem.utils import sorted_by_key # noqa
 from floodsystem.station import MonitoringStation # importing the classes that are made in submodule
 from floodsystem.stationdata import build_station_list # importing all data abt the stations
+
 from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_within_radius
+from floodsystem.geo import rivers_with_station
+from floodsystem.geo import stations_by_river
 
 from haversine import haversine, Unit
 
+
+# Task 1B
 def test_stations_by_distance():
     stations = build_station_list()
     p = (52.2053, 0.1218)
@@ -18,3 +24,29 @@ def test_stations_by_distance():
 
     for each_term in stations:
         assert haversine(p, each_term.coord, unit='km') in distance_list
+
+# Task 1C
+def test_stations_within_radius():
+    stations = build_station_list()
+    centre = (52.2053, 0.1218)
+    r = 10
+    name_list = stations_within_radius(stations, centre, r)
+    for station in stations:
+        if station.name in name_list:
+            assert haversine(centre, station.coord, unit='km') - r < 0
+
+# Task 1D
+def test_rivers_with_station():
+    stations = build_station_list()
+    river_station_list = rivers_with_station(stations)
+    for each_station in stations:
+        assert each_station.river in river_station_list
+    assert len(river_station_list) == len(list(set(river_station_list)))
+
+
+def test_stations_by_river():
+    stations = build_station_list()
+    assert type(stations_by_river(stations)) is dict
+
+
+
