054975caeba6cac0a5bad266d8c61845ad24b09e
ai387 <ai387@cam.ac.uk>
2022-02-16 20:02:18 +0000

    comments

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,3 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
+In a submodule analysis implement a function that given the water level time history (dates, levels) 
+for a station computes a least-squares fit of a polynomial of degree p to water level data. The function 
+should return a tuple of (i) the polynomial object and (ii) any shift of the time (date) axis (see below). 
+The function should have the signature:
+
+def polyfit(dates, levels, p):
+
+Typical usage for a polynomial of degree 3 would be:
+poly, d0 = polyfit(dates, levels, 3)
+where poly is a numpy.poly1d object an d0 is any shift of the date (time) axis.
+'''
